065dd4546357a007902f7ecb11c24ebd17f1fb98
jangyubin010123 <121410330+jangyubin010123@users.noreply.github.com>
2023-01-12 10:54:18 +0900

    Delete 프로그래머스_LV_2 directory

--- 프로그래머스_LV_2/다음_큰_숫자.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    answer = 0
-    temp = n
-    while True:
-        temp += 1
-        if bin(temp)[2:].count("1") == bin(n)[2:].count("1") :
-            answer = temp
-            break
-    return answer
